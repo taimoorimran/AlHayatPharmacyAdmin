666bbe23b5f9eaef306ca787b5aabf035bb5b251
Taimoor Imran <taimoorimran9@gmail.com>
2019-07-04 12:30:34 +0500

    fixes

--- scripts/ng_app.js
+++ scripts/ng_app.js
@@ -13,86 +13,167 @@ angular.module('myModule', [])
     }
     ])
 .controller('myController', function ($scope, $http, $q, $sce, $timeout) {
-    var db = firebase.firestore();
-    $scope.orders = [];
-    $scope.animation_div = true;
-    $scope.login_ = { email: null, password: null, ip: null };
-    $scope.$on('toggle_animation', function (event, data) {
-        $scope.animation_div = data;
-    });
-    db.collection("orders")
-    .onSnapshot(function(querySnapshot) {
-        let data_ = [];
-        querySnapshot.forEach(function(doc) {
-            data_.push(doc.data());
-        });
-        console.log(data_);
-        $scope.$apply(function () {
-            $scope.orders = data_;
-        });
-    });
-    $scope.bindOrderDetail = function (order_detail) {
-        console.log(order_detail);
-        let order_items = [];
-        if(order_detail.prescription_type == 2){
-            angular.forEach(JSON.parse(order_detail.order_items), function(value, key){
-                order_items.push(value);    
-            });
-            order_items = order_items;
-            console.log(order_items);
-        }
-        order_detail.order_items = order_items;
-        $scope.order_detail      = order_detail;
-    };
-    $scope.emptyOrderDetails = function () {
-        $scope.order_detail = null;
-    };
-    $scope.getUsers = function () {
-    }
-    $scope.getSurveys = function () {
+    $scope.safeApply = function(fn) {
+      var phase = this.$root.$$phase;
+      if(phase == '$apply' || phase == '$digest') {
+        if(fn && (typeof(fn) === 'function')) {
+          fn();
+      }
+  } else {
+    this.$apply(fn);
+}
+};
 
+var db = firebase.firestore();
+$scope.orders = [];
+$scope.count = {
+    total_orders : 0,
+    delivery_orders : 0,
+    walkin_orders : 0,
+}
+$scope.animation_div = true;
+$scope.login_ = { email: null, password: null, ip: null };
+$scope.$on('toggle_animation', function (event, data) {
+    $scope.animation_div = data;
+});
+db.collection("orders")
+.onSnapshot(function(querySnapshot) {
+    let data_ = [];
+    let count = {
+        total_orders : 0,
+        delivery_orders : 0,
+        walkin_orders : 0,
     }
-    $scope.setupPage = function () {
-        $scope.UserData = [];
-        $scope.SurveyData = [];
-        $scope.ProcessedUsers = [];
-        $scope.getUsers();
-        $scope.getSurveys();
+    querySnapshot.forEach(function(doc) {
+        let temp = doc.data();
+        if(temp.delivery_option == 0) count.delivery_orders++;
+        else if(temp.delivery_option == 1) count.walkin_orders++;
+        count.total_orders++;
+        data_.push(temp);
+    });
+    console.log(data_);
+    $scope.$apply(function () {
+        console.log(count);
+        $scope.count = count;
+        $scope.orders = data_;
+    });
+});
+$scope.bindOrderDetail = async function (order_detail) {
+    console.log(order_detail);
+    let order_items = [];
+    if(order_detail.prescription_type == 1){
+        let images = [];
+        if(order_detail.prescription_path_one != null){
+            await firebase.storage().ref().child(order_detail.prescription_path_one).getDownloadURL()
+            .then(function(url){
+                $scope.$apply(function() {
+                    order_items.push(url);
+                });
+            })
+            .catch(function(error){
+                switch (error.code) {
+                    case 'storage/object-not-found':
+                    console.error("File doesn't exist");
+                    break;
+                    case 'storage/unauthorized':
+                    console.error("User doesn't have permission to access the object");
+                    break;
+                    case 'storage/canceled':
+                    console.error("User canceled the upload");
+                    break;
+                    case 'storage/unknown':
+                    console.error("Unknown error occurred, inspect the server response");
+                    break;
+                }
+            })
+        }
+        if(order_detail.prescription_path_two != null){
+            await firebase.storage().ref().child(order_detail.prescription_path_two).getDownloadURL()
+            .then(function(url){
+                $scope.$apply(function() {
+                    order_items.push(url);
+                });
+            })
+            .catch(function(error){
+                switch (error.code) {
+                    case 'storage/object-not-found':
+                    console.error("File doesn't exist");
+                    break;
+                    case 'storage/unauthorized':
+                    console.error("User doesn't have permission to access the object");
+                    break;
+                    case 'storage/canceled':
+                    console.error("User canceled the upload");
+                    break;
+                    case 'storage/unknown':
+                    console.error("Unknown error occurred, inspect the server response");
+                    break;
+                }
+            })
+        }
+        order_detail.order_items = images;
     }
-    $scope.getItemIcon = function (itemType) {
-        if (itemType == 'TV') return 'icon-screen-desktop';
-        else if (itemType == 'Mobile') return 'icon-screen-smartphone';
-        else if (itemType == 'Magzine') return 'icon-book-open';
-        else if (itemType == 'Newspaper') return 'icon-book-open';
-        else return 'icon-home';
+    else if(order_detail.prescription_type == 2){
+        if(typeof order_detail.order_items != 'object') order_detail.order_items = JSON.parse(order_detail.order_items);
+        angular.forEach(order_detail.order_items, function(value, key){
+            order_items.push(value);    
+        });
+        order_items = order_items;
+    }
+    order_detail.order_items = order_items;
+    $scope.safeApply(function () {
+        $scope.order_detail = order_detail;
+    });        
+};
+$scope.emptyOrderDetails = function () {
+    $scope.order_detail = null;
+};
+$scope.getUsers = function () {
+}
+$scope.getSurveys = function () {
 
-    };
-    $scope.checkImage = function (url) {
-        var http = new XMLHttpRequest();
-        http.open('HEAD', url, false);
-        http.send();
-        return http.status != 404;
+}
+$scope.setupPage = function () {
+    $scope.UserData = [];
+    $scope.SurveyData = [];
+    $scope.ProcessedUsers = [];
+    $scope.getUsers();
+    $scope.getSurveys();
+}
+$scope.getItemIcon = function (itemType) {
+    if (itemType == 'TV') return 'icon-screen-desktop';
+    else if (itemType == 'Mobile') return 'icon-screen-smartphone';
+    else if (itemType == 'Magzine') return 'icon-book-open';
+    else if (itemType == 'Newspaper') return 'icon-book-open';
+    else return 'icon-home';
+
+};
+$scope.checkImage = function (url) {
+    var http = new XMLHttpRequest();
+    http.open('HEAD', url, false);
+    http.send();
+    return http.status != 404;
+}
+$scope.login = function () {
+    if ($scope.login_.email != 'admin@markematics.net') {
+        alert('Wrong Username!');
+        return;
     }
-    $scope.login = function () {
-        if ($scope.login_.email != 'admin@markematics.net') {
-            alert('Wrong Username!');
-            return;
-        }
-        if ($scope.login_.password != '123456789') {
-            alert('Wrong Password!');
-            return;
-        }
-        $scope.showAnimation();
-        $scope.isLoggedIn = true;
-        $scope.goBack();
+    if ($scope.login_.password != '123456789') {
+        alert('Wrong Password!');
+        return;
     }
-    $scope.logout = function () {
-        $scope.isLoggedIn = false;
-        $scope.animationShowed = false;
-    }
-    $scope.showAnimation = function () {
-        $timeout(function () {
-            $scope.animationShowed = true;
-        }, 6000);
-    }
+    $scope.showAnimation();
+    $scope.isLoggedIn = true;
+    $scope.goBack();
+}
+$scope.logout = function () {
+    $scope.isLoggedIn = false;
+    $scope.animationShowed = false;
+}
+$scope.showAnimation = function () {
+    $timeout(function () {
+        $scope.animationShowed = true;
+    }, 6000);
+}
 });
