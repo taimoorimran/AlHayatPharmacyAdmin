703e4b38395706152050a165cc001ada3a0aa5f5
Taimoor Imran <taimoorimran9@gmail.com>
2020-07-16 11:51:28 +0500

    mm

---
+++ ng_scripts/firebase_.js
@@ -0,0 +1,9 @@
+firebase.initializeApp({
+    apiKey: "AIzaSyDggChfryFxdz8obYEgYjQZnCWYefqr1FM",
+    authDomain: "alhayatpharmacy-47bd1.firebaseapp.com",
+    databaseURL: "https://alhayatpharmacy-47bd1.firebaseio.com",
+    projectId: "alhayatpharmacy-47bd1",
+    storageBucket: "alhayatpharmacy-47bd1.appspot.com",
+    messagingSenderId: "60734804118",
+    appId: "1:60734804118:web:f64ef45520c25497"
+});
